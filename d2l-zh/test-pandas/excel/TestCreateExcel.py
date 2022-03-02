0624162bd31e4e010239159fae170fc4e5dfb324
Hirqs <569543446@qq.com>
2022-03-02 19:56:24 +0800

    meishenme

--- d2l-zh/test-pandas/excel/TestCreateExcel.py
+++ d2l-zh/test-pandas/excel/TestCreateExcel.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import openpyxl
 
 result = pd.DataFrame({'序号':[1,2,3],'姓名':['张三','李四','王五']})  # 字典
 result = result.set_index('序号')      # 将序号设置为索引
 print(result)
 result.to_excel(r'D:\create.xls')
+
+
 
---
+++ d2l-zh/test-pandas/excel/TestDataFrame.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df=pd.DataFrame({'x':[1,2,4],'y':[8,16,32]})
+# df=df.set_index('x')                            # 将‘x’设置为索引
+print(df)
+
