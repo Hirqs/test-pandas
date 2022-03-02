fafc32a8812f79174174508e4bac84298369d9d1
Hirqs <569543446@qq.com>
2022-03-02 16:44:18 +0800

    read2 create2

--- d2l-zh/test-pandas/excel/TestCreateExcel.py
+++ d2l-zh/test-pandas/excel/TestCreateExcel.py
@@ -1,5 +1,7 @@
 import pandas as pd
-import xlwt
-result=pd.DataFrame({'姓名':['张三','李四','王五']})
-# result=pd.DataFrame({'序号':[1,2,3],'姓名':['张三','李四','王五']})
-result.to_excel('D:\\test_create.xls')
+
+result = pd.DataFrame({'序号':[1,2,3],'姓名':['张三','李四','王五']})  # 字典
+result = result.set_index('序号')      # 将序号设置为索引
+print(result)
+result.to_excel(r'D:\create.xls')
+
